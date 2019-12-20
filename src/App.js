d8f98f637c0d55f0f8adb430957ad4349c2001bd
Joshua Yoes <>
2019-12-19 21:33:30 -0800

    Added request and styling

--- src/App.js
+++ src/App.js
@@ -3,24 +3,32 @@ import './App.scss';
 import $ from 'jquery';
 
 function App() {
-  useEffect(() => {
+  const [quote, setQuote] = useState('First quote');
+  
+  const loadQuote = () => {
     let apiKey = process.env.PAPER_QUOTES_API_KEY;
+    
+    //http://paperquotes.com/api-docs/#simple-usage
     $.ajax({ 
       type : "GET", 
       url : "https://cors-anywhere.herokuapp.com/https://api.paperquotes.com/apiv1/quotes/?lang=en", 
       beforeSend: function(xhr){xhr.setRequestHeader('Authorization', `Token {${apiKey}}`);},
       success : function(result) { 
-          console.log(result.results); 
+        console.log(result.results); 
+        setQuote(result.results[0])
       }, 
       error : function(result) { 
-          console.log(result.results);
+        console.log(result.results);
       } 
-    }); 
-  });
+    });
+  };
 
   return (
-    <div className="App">
+    <div id="quote-box">
       <h1>Random Quote Generator</h1>
+      <p id="text">{quote.quote}</p>
+      <p id="author">{quote.author}</p>
+      <button id="new-quote" onClick={loadQuote}>New Quote</button>
     </div>
   );
 }
