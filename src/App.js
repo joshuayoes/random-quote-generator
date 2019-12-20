d3fefa8fa70908498ec959a70de4736c427997a5
Joshua Yoes <>
2019-12-19 20:55:55 -0800

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import './App.scss';
+import $ from 'jquery';
 
 function App() {
+  useEffect(() => {
+    let apiKey = process.env.PAPER_QUOTES_API_KEY;
+    $.ajax({ 
+      type : "GET", 
+      url : "https://cors-anywhere.herokuapp.com/https://api.paperquotes.com/apiv1/quotes/?lang=en", 
+      beforeSend: function(xhr){xhr.setRequestHeader('Authorization', `Token {${apiKey}}`);},
+      success : function(result) { 
+          console.log(result.results); 
+      }, 
+      error : function(result) { 
+          console.log(result.results);
+      } 
+    }); 
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Random Quote Generator</h1>
     </div>
   );
 }
