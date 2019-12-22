08cda15d6d9b09d50a3ad207b87f303138974236
Joshua Yoes <>
2019-12-22 02:38:21 -0800

    Implemented user stories with Ron Swanson API

--- src/App.js
+++ src/App.js
@@ -1,34 +1,51 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import './App.scss';
 import $ from 'jquery';
 
 function App() {
-  const [quote, setQuote] = useState('First quote');
+  const [quote, setQuote] = useState('Loading');
   
-  const loadQuote = () => {
-    let apiKey = process.env.PAPER_QUOTES_API_KEY;
-    
-    //http://paperquotes.com/api-docs/#simple-usage
-    $.ajax({ 
-      type : "GET", 
-      url : "https://cors-anywhere.herokuapp.com/https://api.paperquotes.com/apiv1/quotes/?lang=en", 
-      beforeSend: function(xhr){xhr.setRequestHeader('Authorization', `Token {${apiKey}}`);},
-      success : function(result) { 
-        console.log(result.results); 
-        setQuote(result.results[0])
-      }, 
-      error : function(result) { 
-        console.log(result.results);
-      } 
-    });
-  };
+  const fetchQuote = async () => {
+    //See GitHub for API documentation
+    //https://github.com/jamesseanwright/ron-swanson-quotes
+
+    const response = await fetch('https://ron-swanson-quotes.herokuapp.com/v2/quotes');
+    const json = await response.json();
+    const output = json[0]
+    setQuote(output);
+    tweetUrl(output);
+  }
+
+  const tweetUrl = (output) => {
+    const twitterUrl = 'http://twitter.com/intent/tweet/?text=';
+    const tagLine = ' - Ron Swanson'
+    const regex = /\s/gi;
+    const tweetQuery = output
+      .concat(tagLine)
+      .replace(regex, '%20');
+
+    //Update the tweet button with new quote
+    $('#tweet-quote').attr("href", twitterUrl.concat(tweetQuery));
+  }
+
+  //Grabs first quote on load
+  window.onload = fetchQuote;
 
   return (
     <div id="quote-box">
-      <h1>Random Quote Generator</h1>
-      <p id="text">{quote.quote}</p>
-      <p id="author">{quote.author}</p>
-      <button id="new-quote" onClick={loadQuote}>New Quote</button>
+      <h1>Ron Swanson Quotes</h1>
+      <p id="text">{quote}</p>
+      <p id="author">Ron Swanson</p>
+      <button 
+        id="new-quote" 
+        onClick={fetchQuote}>
+          New Quote
+      </button>
+      <a 
+        id="tweet-quote"
+        href="https://twitter.com/intent/tweet/">
+        Tweet
+      </a>
     </div>
   );
 }
